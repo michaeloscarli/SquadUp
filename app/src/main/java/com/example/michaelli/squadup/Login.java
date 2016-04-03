3efb022bf9057ce62c014876f72eb360ad3910c6
Michael Li <mlli2@illinois.edu>
2016-04-03 16:57:06 -0500

    Added some test queries for sql db

--- app/src/main/java/com/example/michaelli/squadup/Login.java
+++ app/src/main/java/com/example/michaelli/squadup/Login.java
@@ -3,12 +3,15 @@ package com.example.michaelli.squadup;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.util.Log;
 import com.bailey.mobile.squadup.R;
+import android.database.Cursor;
 
 /**
  * Created by michaelli on 3/29/16.
@@ -22,6 +25,11 @@ public class Login extends Activity{
         setContentView(R.layout.login);
         //create dictionary helper that accesses/creates database
         dictionaryHelper = new DictionaryHelper(this);
+        SQLiteDatabase test = dictionaryHelper.getWritableDatabase();
+        Log.d("Login", dictionaryHelper.getDatabaseName());
+        Cursor c = test.rawQuery("SELECT name FROM sqlite_master WHERE type='table'", null);
+        Log.d("Login", c.getString(0));
+
 
     }
 }
