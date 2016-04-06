2ebdc521015470cec798ae620be201d424b48ef0
Michael Li <mlli2@illinois.edu>
2016-04-05 19:21:17 -0500

    Prepared to send database IDs to next view

--- app/src/main/java/com/example/michaelli/squadup/GameHistory.java
+++ app/src/main/java/com/example/michaelli/squadup/GameHistory.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
@@ -31,9 +34,24 @@ public class GameHistory extends Activity{
         SQLiteDatabase db = dictionaryHelper.getReadableDatabase();
         Cursor cursor = db.rawQuery("select * from GAMES", null);
         String[] fields = new String[] {"Opponent", "Date"};
-        SimpleCursorAdapter simpleCursorAdapter;
-        simpleCursorAdapter = new SimpleCursorAdapter(this,android.R.layout.simple_list_item_2, cursor,  fields, new int[] { android.R.id.text1,android.R.id.text2},0);
+        final SimpleCursorAdapter simpleCursorAdapter;
+        simpleCursorAdapter = new SimpleCursorAdapter(this,android.R.layout.simple_list_item_2, cursor,  fields, new int[] {android.R.id.text1,android.R.id.text2},0);
         ListView listView = (ListView) findViewById(R.id.notifications_list);
-        listView.setAdapter(simpleCursorAdapter );
+        listView.setAdapter(simpleCursorAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Cursor cursor = (Cursor) simpleCursorAdapter.getItem(position);
+                int gameID = Integer.parseInt(cursor.getString(cursor.getColumnIndex("_id")));
+                Log.d("Position",Integer.toString(gameID));
+//                User user = adapter.getItem(position);
+//                Bundle b = new Bundle();
+//                b.putParcelable("user", user);
+//
+//                Intent i = new Intent(context, OtherActivity.class);
+//                i.putExtra("bundle", b);
+//                startActivity(i);
+            }
+        });
     }
 }
