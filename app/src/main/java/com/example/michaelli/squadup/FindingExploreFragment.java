5042ba1d6b421433379baedb177fd757b3fbf64f
Connie Fan <cfan12@illinois.edu>
2016-04-05 19:33:37 -0500

    intro frag pages

---
+++ app/src/main/java/com/example/michaelli/squadup/FindingExploreFragment.java
@@ -0,0 +1,37 @@
+package com.example.michaelli.squadup;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bailey.mobile.squadup.R;
+
+public class FindingExploreFragment extends Fragment {
+    ImageView imageView;
+    TextView introExplore;
+    TextView introAroundYou;
+    ImageView introChannels;
+    Button introFinish;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_intro_page_4, container, false);
+
+        imageView = (ImageView) rootView.findViewById(R.id.intro_compass);
+
+        introFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finish();
+            }
+        });
+
+        return rootView;
+    }
+}
